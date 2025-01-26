33631e587e2c2a2c376dc4a13d6dadf6626a6097
Benjamin Patton <bass41992ben@gmail.com>
2025-01-26 14:28:02 -0500

    some ui updates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,11 @@ import { cn } from "@/lib/utils";
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isToggled, setIsToggled] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsToggled(!isToggled);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -88,7 +93,123 @@ export default function Header() {
             </div>
           </div>
         </nav>
+        <button
+          aria-label="Open mobile nav"
+          className="p-2 md:hidden"
+          onClick={toggleMobileMenu}
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={cn(
+              "relative overflow-visible text-inherit transition-transform duration-500 ease-out",
+              { "rotate-90": isToggled }
+            )}
+          >
+            <path
+              d="M20.4853 3.51473L3.51477 20.4853"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={cn("absolute origin-center translate-y-1 rotate-45", {
+                "translate-y-0 rotate-90": isToggled,
+              })}
+            />
+            <path
+              d="M20.4853 20.4853L3.51477 3.51471"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={cn(
+                "absolute origin-center -translate-y-1 -rotate-45",
+                {
+                  "translate-y-0 rotate-90": isToggled,
+                }
+              )}
+            />
+          </svg>
+        </button>
       </div>
+      {isToggled && (
+        <nav className="fade animate-fadeSm fixed top-0 h-screen w-full bg-black/50 px-6 pt-24 text-white backdrop-blur-2xl ease-out md:hidden">
+          <nav className="flex gap-10 [@media(min-width:400px)]:gap-20">
+            <div className="flex flex-col">
+              <div className="mb-6 w-full">
+                <ul>
+                  <li className="my-2 w-full">
+                    <a className="text-xl font-medium" href="/">
+                      Home
+                    </a>
+                  </li>
+                  <li className="my-2 w-full">
+                    <a className="text-xl font-medium" href="/workshops">
+                      Workshops
+                    </a>
+                  </li>
+                  {/* <li className="my-2 w-full">
+                    <a className="text-xl font-medium" href="/tips">
+                      Tips
+                    </a>
+                  </li> */}
+                  {/* <li className="my-2 w-full">
+                    <a className="text-xl font-medium" href="/tutorials">
+                      Tutorials
+                    </a>
+                  </li> */}
+                  <li className="my-2 w-full">
+                    <a className="text-xl font-medium" href="/posts">
+                      Posts
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="mt-10 flex gap-4 *:inline *:w-1/2 *:text-center *:text-base">
+            {/* <a
+              aria-label="Newsletter Sign Up"
+              className="group flex w-max items-center gap-1 rounded-full border-white bg-neutral-200 px-3.5 py-2 text-sm font-semibold text-neutral-800 transition-colors sm:hover:bg-white sm:hover:text-black"
+              href="https://faequeite.ck.page/eb37af7d8f"
+            >
+              Newsletter
+            </a> */}
+            {/* {user.state.user ? (
+					<span
+						aria-label="Profile"
+						role="button"
+						tabIndex={0}
+						className="group flex w-max items-center gap-1 rounded-full border-white bg-neutral-200 px-3.5 py-2 text-sm font-semibold text-neutral-800 transition-colors sm:hover:bg-white sm:hover:text-black"
+						onClick={handleSignOut}
+						onKeyDown={handleSignOut}>Logout</span
+					>
+				) : (
+					<Dialog>
+						<DialogTrigger
+							className={cn(
+								'group flex w-max items-center gap-1 rounded-full border-white bg-neutral-200 px-3.5 py-2 text-sm font-semibold transition-colors sm:hover:bg-white sm:hover:text-black',
+								{ 'text-white': !isScrolled, 'text-black': isScrolled }
+							)}
+						>
+							<span aria-label="Profile">Login</span>
+						</DialogTrigger>
+						<DialogContent>
+							<DialogHeader>
+								<DialogDescription>
+									{/* {@render modalContent()}
+								</DialogDescription>
+							</DialogHeader>
+						</DialogContent>
+					</Dialog>
+				)} */}
+          </div>
+        </nav>
+      )}
+      <div className="pointer-events-none absolute left-0 top-0 h-screen w-full select-none bg-black/10 opacity-0 backdrop-blur-sm transition-opacity duration-500 ease-out"></div>
     </header>
   );
 }
